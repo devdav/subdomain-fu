35b0baa8339679034971d08634cb843287457087
David Edwards <david.edwards.gm@gmail.com>
2026-06-07 19:11:56 +1000

    fix

--- lib/subdomain_fu/url_rewriter.rb
+++ lib/subdomain_fu/url_rewriter.rb
@@ -1,7 +1,7 @@
 require 'action_dispatch/routing/route_set'
 
 module SubdomainFuUrlForMonkeyPatch
-  def url_for(options, route_name = nil, url_strategy = UNKNOWN, method_name = nil, reserved = RESERVED_OPTIONS)
+  def url_for(options, route_name = nil, url_strategy = UNKNOWN, method_name = nil, reserved = ActionDispatch::Routing::RouteSetRESERVED_OPTIONS)
     if SubdomainFu.needs_rewrite?(options[:subdomain], (options[:host] || (@request && @request.host_with_port))) || options[:only_path] == false
       options[:only_path] = false if SubdomainFu.override_only_path?
       options[:host] = SubdomainFu.rewrite_host_for_subdomains(options.delete(:subdomain), options[:host] || (@request && @request.host_with_port))
